b0b802a84d4e859cf510d678d6c68ff521557923
PLECITO <116316034+PLECITO@users.noreply.github.com>
2023-01-08 20:44:00 +0000

    actualizacion hasta la clase n° 22

--- Programar/Programar/mokepon/js/mokepon.js
+++ Programar/Programar/mokepon/js/mokepon.js
@@ -1,5 +1,5 @@
 function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById("boton-macota")
+    let botonMascotaJugador = document.getElementById("boton-mascota")
     botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
 
@@ -7,16 +7,33 @@ function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
     let inputRatigueya = document.getElementById("ratigueya")
-    
+    let spanMascotaJugador = document.getElementById("mascota-jugador")
+
     if (inputHipodoge.cheked) {
-        alert("Seleccinaste a Hipodoge")
+        spanMascotaJugador.innerHTML = "Hipodoge"
     } else if (inputCapipepo.cheked) {
-        alert("Seleccionaste a Capipepo")
+        spanMascotaJugador.innerHTML = "Capipepo"
     } else if (inputRatigueya.cheked) {
-        alert("Seleccionaste a Ratigueya")
+        spanMascotaJugador.innerHTML = "Ratigueya"
     } else {
-        alert("Selecciona una mascota")
+        alert("Por favor selecciona una mascota")
+    }
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = "Hipodoge"
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    } else {
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
     }
 }
 
-window.addEventListener("load", iniciarJuego)
+
+window.addEventListener("load", iniciarJuego) 
